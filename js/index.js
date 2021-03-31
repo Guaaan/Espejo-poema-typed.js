949fbde682bdaf3a482313b301228d50bd8e6154
Guaaan <72053761+Guaaan@users.noreply.github.com>
2021-03-31 17:39:01 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: false,
+    delay: 2
+});
+
+typewriter.typeString('ante el viejo grabado amarillo <br>de la piscina bajo el sol<br> Entre el pasto de verano<br>el armazón blanco de la mesa de vidrio<br>junto la sangre de Cándida Margarita<br>sentía el olor del limonero<br>el pescado friéndose lejos<br>Y el vino malteñido casi tierra<br>Soga de esparto para que parezca un suicidio<br>a la colina del fondo resuena donde llegó el grito<br>y se pierde hasta siempre un rato<br><br>de noche, cuando se sienten temblar las maderas<br>arrastrares de talón, ávidos crepitares<br>que paran la oreja como un perro que espera su niño<br>Fuegos de oír un fantasma conocido<br>Cándida aprovecha y me cuenta de los gusanos que la acosan<br>y a veces me narra de espectros que llama colegas<br>y una disolución ante diversas instancias<br>grabado que todos los días pinta y despinta  <br><br>ruinoso recordatorio<br>de los puertas adentro, atentados cometidos  ')
+    .start();
